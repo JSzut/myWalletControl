3a9a8c21c8c1310b15c0540e628db32c6214f0a0
Jakub Szut <j.szut@si-research.eu>
2017-08-08 00:05:46 +0200

    Main pages: Profile, myWallets and Setting added. All pages are Navigation pages, the main structure is TabbedPage. Button adding new wallets buttons added.

---
+++ myWalletXAML/myWalletXAML/WalletsPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace myWalletXAML
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class WalletsPage : ContentPage
+	{
+        int i = 0;
+		public WalletsPage ()
+		{
+			InitializeComponent ();
+		}
+
+        async void onNewWalletButtonClicked(object sender, EventArgs e)
+        {
+            var button = new Button
+            {
+                Text = $"Wallet {i++}",
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+            walletsStack.Children.Add(button);
+            await Navigation.PushAsync(new newWalletPage());
+        }
+		
+    }
+}
---
+++ myWalletXAML/myWalletXAML/newWalletPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace myWalletXAML
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class newWalletPage : ContentPage
+	{
+		public newWalletPage ()
+		{
+			InitializeComponent ();
+		}
+        async void OnBackButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
